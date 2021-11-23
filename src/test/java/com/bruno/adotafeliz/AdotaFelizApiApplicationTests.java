f0bb08d1f4717b6b48182f7b5cb84f41be0bd3a0
BrunoGTI <brunoh14pb@gmail.com>
2021-11-23 10:05:05 -0300

    commit initial

---
+++ src/test/java/com/bruno/adotafeliz/AdotaFelizApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.adotafeliz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdotaFelizApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
